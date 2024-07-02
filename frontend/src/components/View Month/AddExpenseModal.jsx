c9029f5bd2b9a71affa4e9031d5a61d4f98ed9cf
R-Tramsport <ryantram6@gmail.com>
2024-07-02 15:41:21 +1000

    added rough text inputs to the add expense modal. Currently calls a POST request to a 400 error. Needs to be sussed

--- frontend/src/components/View Month/AddExpenseModal.jsx
+++ frontend/src/components/View Month/AddExpenseModal.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import { AiOutlineClose } from "react-icons/ai";
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const AddExpenseModal = ({ expense, onClose }) => {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [category, setCategory] = useState("");
+    const [amount, setAmount] = useState(0);
+
+    const handleSubmit = () => {
+        const data = {
+            title,
+            description,
+            category,
+            amount,
+            "isSpecial": false
+        }
+        axios
+            .post(`http://localhost:5555/transactions/`, data)
+            .catch((error) => {
+                alert("An error occured. Please check console.");
+                console.log(error);
+            })
+    }
+
     return (
         <div 
             className="fixed top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
-            style={{backgroundColor: "black", opacity: "0.80"}}
+            style={{backgroundColor: "black", opacity: "0.9"}}
             onClick={onClose}    
         >
             <div
@@ -19,6 +43,39 @@ const AddExpenseModal = ({ expense, onClose }) => {
                 <h2>
                     Add Expense
                 </h2>
+                <div>
+                    Title
+                    <input 
+                        name="title" 
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div>
+                    Description
+                    <input 
+                        name="description" 
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                </div>
+                <div>
+                    Category
+                    <input 
+                        name="category" 
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                    />
+                </div>
+                <div>
+                    Amount
+                    <input 
+                        name="amount" 
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                    />
+                </div>
+                <button onClick={() => handleSubmit()}>Submit</button>
             </div>
         </div>
     )
