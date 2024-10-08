6acc08c558fffa89411adeee9956a20adda99e7a
R-Tramsport <ryantram6@gmail.com>
2024-10-08 16:54:02 +1100

    Added rough links in the navbar for testing. Home to Month and Month to home work, however month to month does not update transactions.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
+
 const NavBar = () => {
     return (
         <div className='navbar'>
             <div className='title'>Financial tracker</div>
+            <Link to="/">Home</Link>
+            <Link to="/month/01/2024">January</Link>
+            <Link to="/month/07/2024">July</Link>
         </div>
     );
 }
